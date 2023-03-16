462f01f350093a036235e2c2683725c4266668c2
Blargtille <eloise.grn@gmail.com>
2023-03-16 16:46:52 +0100

    ajouts TP5

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,10 +5,18 @@ namespace App\Controller;
 use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route("/", name: "main_")]
 class MainController extends AbstractController
 {
-    #[Route("/", name: "main_home")]
+    #[Route("", name: "home")]
     public function home(){
         return $this->render('main/home.html.twig');
+    }
+    #[Route("/aboutus", name: "aboutus")]
+    public function aboutUs(){
+        $lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda autem, dolorem doloribus 
+        eius fugiat id ipsum itaque laboriosam, minus, nemo nostrum odio quae quo rem repellat reprehenderit 
+        sint tempore voluptatum.";
+        return $this->render('main/aboutUs.html.twig',['lorem' => $lorem]);
     }
 }
